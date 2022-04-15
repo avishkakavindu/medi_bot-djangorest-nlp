99eaa6d52e0cb4fa1b0e6ce78d915c5753f71922
avishkakavindu <avishkakavindud@gmail.com>
2022-04-15 11:36:51 +0530

    Heroku required files

--- medi_bot_api/settings.py
+++ medi_bot_api/settings.py
@@ -28,7 +28,10 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'medibot-api.herokuapp.com',
+    '127.0.0.1'
+]
 
 
 # Application definition
